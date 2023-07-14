f430835022628467ec687af9022ced7d7142d06d
CherezBereg <117637101+CherezBereg@users.noreply.github.com>
2023-07-15 01:35:50 +0300

    error correction after review 1

--- homework.py
+++ homework.py
@@ -1,28 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        information: str = ('Тип тренировки: {}; '
+                            + 'Длительность: {:.3f} ч.; '
+                            + 'Дистанция: {:.3f} км; '
+                            + 'Ср. скорость: {:.3f} км/ч; '
+                            + 'Потрачено ккал: {:.3f}.')
+        return information.format(self.training_type,
+                                  self.duration,
+                                  self.distance,
+                                  self.speed,
+                                  self.calories)
 
 
 class Training:
     """Базовый класс тренировки."""
+
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
     MIN_IN_H: int = 60
@@ -48,7 +52,9 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        pass
+        raise NotImplementedError(
+            'Переопределите метод get_spent_calories в {}'
+            .format(type(self).__name__))
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
@@ -60,6 +66,7 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
+
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
@@ -72,6 +79,7 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+
     CALORIES_WEIGHT_MULTIPLIER: float = 0.035
     CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
     KMH_IN_MSEC: float = 0.278
@@ -97,6 +105,7 @@ class SportsWalking(Training):
 
 class Swimming(Training):
     """Тренировка: плавание."""
+
     LEN_STEP: float = 1.38
     CALORIES_MEAN_SPEED_SHIFT: float = 1.1
     CALORIES_WEIGHT_MULTIPLIER: float = 2
@@ -111,8 +120,8 @@ class Swimming(Training):
         self.count_pool = count_pool
 
     def get_mean_speed(self):
-        return self.length_pool * self.count_pool / \
-            self.M_IN_KM / self.duration
+        return (self.length_pool * self.count_pool
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self):
         return ((self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
@@ -120,16 +129,19 @@ class Swimming(Training):
                 * self.weight * self.duration)
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str,
+                 data: list[tuple[str, list[int]]]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    activity_type: dict = {
+    activity_type: dict[str, type[Training]] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
     }
-
-    if workout_type in activity_type:
-        return activity_type[workout_type](*data)
+    try:
+        if workout_type in activity_type:
+            return activity_type[workout_type](*data)
+    except ValueError:
+        print('Некорректный пакет данных')
 
 
 def main(training: Training) -> None:
